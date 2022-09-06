1d7b1b27fd92e7ccc4aef4c85b57e39a49dbf06c
Ashokzarmariya <ashokzarmariya47@gmail.com>
2022-09-06 10:40:20 +0530

    fixed length issue

--- src/components/HomePage/UserChat.jsx
+++ src/components/HomePage/UserChat.jsx
@@ -21,7 +21,7 @@ const UserChat = ({
           {isChat && <p className="text-sm">{sentTime}</p>}
         </div>
         <div className="flex justify-between items-center">
-           <p className={`${isChat && message.length>0?"visible":"invisible"}`}>{message.length>15? message.slice(0,15)+"...":message}</p>
+           <p className={`${isChat && message?.length>0?"visible":"invisible"}`}>{message?.length>15? message.slice(0,15)+"...":message}</p>
           <div className="flex space-x-2 items-center">
             {isChat && isNotification && notification > 0 && (
               <p className="text-xs p-1 px-2 text-white bg-green-500 rounded-full">
@@ -29,7 +29,7 @@ const UserChat = ({
               </p>
             )}
             <div className="flex justify-end flex-1">
-              <AiOutlineDown className="hidden group-hover:block" />
+              {isChat && <AiOutlineDown className="hidden group-hover:block" />}
             </div>
             
           </div>
